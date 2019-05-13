67d3ad68270a2fe4f96795a2096cbbd6c9f13cb4
Max <sibo_jian_29903549@163.com>
2019-05-13 16:50:03 +0800

    [功能]($\Demo & $\Sources)
    添加 Cocoa Touch Framework 项目主体，对项目文件目录结构调整。
    添加 Single View App 项目主体，对项目文件目录结构调整。
    添加 JSSwifter.xcodeproj 管理上述项目主体。

---
+++ Demo/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSSwifter-Demo
+//
+//  Created by Max on 2019/5/13.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sources/JSSwifter.h
@@ -0,0 +1,19 @@
+//
+//  JSSwifter.h
+//  JSSwifter
+//
+//  Created by Max on 2019/5/13.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSSwifter.
+FOUNDATION_EXPORT double JSSwifterVersionNumber;
+
+//! Project version string for JSSwifter.
+FOUNDATION_EXPORT const unsigned char JSSwifterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSSwifter/PublicHeader.h>
+
+

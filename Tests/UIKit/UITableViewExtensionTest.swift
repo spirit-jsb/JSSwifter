2f70483ba3ca69ba9301f737b23f0be85bcacd15
Max <sibo_jian_29903549@163.com>
2019-05-15 18:07:04 +0800

    [测试]($UITableViewExtensionTest)
    编写 UITableView 拓展测试。

---
+++ Tests/UIKit/UITableViewExtensionTest.swift
@@ -0,0 +1,39 @@
+//
+//  UITableViewExtensionTest.swift
+//  JSSwifter-Tests
+//
+//  Created by Max on 2019/5/15.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import XCTest
+
+class UITableViewExtensionTest: XCTestCase {
+
+    func test_register() {
+        let tableView = UITableView(frame: .zero, style: .plain)
+
+        tableView.registerCell(TestTableViewCell.self)
+        tableView.registerHeaderFooterView(TestTableViewHeaderFooterView.self)
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestTableViewCell") as? TestTableViewCell
+        let headerFooterView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "TestTableViewHeaderFooterView") as? TestTableViewHeaderFooterView
+        XCTAssertNotNil(cell)
+        XCTAssertNotNil(headerFooterView)
+    }
+    
+    func test_dequeue() {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        
+        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: "TestTableViewCell")
+        tableView.register(TestTableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "TestTableViewHeaderFooterView")
+        
+        let cell = tableView.dequeueCell() as TestTableViewCell
+        let headerFooterView = tableView.dequeueHeaderFooterView() as TestTableViewHeaderFooterView
+        XCTAssertNotNil(cell)
+        XCTAssertNotNil(headerFooterView)
+    }
+}
+
+private class TestTableViewCell: UITableViewCell {  }
+private class TestTableViewHeaderFooterView: UITableViewHeaderFooterView {  }

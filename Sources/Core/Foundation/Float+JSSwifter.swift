afa0ca4793ce9417688eece8fb3319673dc6c978
Max <sibo_jian_29903549@163.com>
2019-05-16 18:27:14 +0800

    [功能]($Float+JSSwifter)
    添加 Float+JSSwifter 拓展。
    
    - var int: Int
    - var double: Double
    - var cgFloat: CGFloat
    
    - func ** (lhs: Float, rhs: Float) -> Float
    - public prefix func √ (float: Float) -> Float

---
+++ Sources/Core/Foundation/Float+JSSwifter.swift
@@ -0,0 +1,38 @@
+//
+//  Float+JSSwifter.swift
+//  JSSwifter
+//
+//  Created by Max on 2019/5/16.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import Foundation
+
+public extension Float {
+    
+    // MARK:
+    var int: Int {
+        return Int(self)
+    }
+    
+    var double: Double {
+        return Double(self)
+    }
+    
+    var cgFloat: CGFloat {
+        return CGFloat(self)
+    }
+}
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ** : PowerPrecedence
+
+func ** (lhs: Float, rhs: Float) -> Float {
+    return pow(lhs, rhs)
+}
+
+prefix operator √
+
+public prefix func √ (float: Float) -> Float {
+    return sqrt(float)
+}

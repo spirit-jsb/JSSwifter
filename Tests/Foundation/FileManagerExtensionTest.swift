7599440d2c3025c8981f78cf05a7efb93c8d68b2
Max <sibo_jian_29903549@163.com>
2019-05-18 00:52:44 +0800

    [测试]($FileManagerExtensionTest)
    添加相应测试。

---
+++ Tests/Foundation/FileManagerExtensionTest.swift
@@ -0,0 +1,46 @@
+//
+//  FileManagerExtensionTest.swift
+//  JSSwifter-Tests
+//
+//  Created by Max on 2019/5/18.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import XCTest
+@testable import JSSwifter
+
+class FileManagerExtensionTest: XCTestCase {
+    
+    func test_document() {
+        let path = FileManager.default.documentPath
+        let url = FileManager.default.documentURL
+        
+        XCTAssertNotNil(path)
+        XCTAssertNotNil(url)
+        
+        XCTAssertTrue(path!.contains("Document"))
+        XCTAssertTrue(url!.absoluteString.contains("Document"))
+    }
+    
+    func test_library() {
+        let path = FileManager.default.libraryPath
+        let url = FileManager.default.libraryURL
+        
+        XCTAssertNotNil(path)
+        XCTAssertNotNil(url)
+        
+        XCTAssertTrue(path!.contains("Library"))
+        XCTAssertTrue(url!.absoluteString.contains("Library"))
+    }
+    
+    func test_caches() {
+        let path = FileManager.default.cachesPath
+        let url = FileManager.default.cachesURL
+        
+        XCTAssertNotNil(path)
+        XCTAssertNotNil(url)
+        
+        XCTAssertTrue(path!.contains("Caches"))
+        XCTAssertTrue(url!.absoluteString.contains("Caches"))
+    }
+}

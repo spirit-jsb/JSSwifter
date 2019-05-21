2c3ee076d5c3f4d1cb9d612bce62079d7d33bf21
Max <sibo_jian_29903549@163.com>
2019-05-21 15:31:47 +0800

    [功能]($Locale+JSSwifter)
    添加 Locale+JSSwifter 拓展。
    
    - static var posix: Locale

---
+++ Sources/Core/Foundation/Locale+JSSwifter.swift
@@ -0,0 +1,17 @@
+//
+//  Locale+JSSwifter.swift
+//  JSSwifter
+//
+//  Created by Max on 2019/5/21.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import Foundation
+
+public extension Locale {
+    
+    // MARK
+    static var posix: Locale {
+        return Locale(identifier: "en_US_POSIX")
+    }
+}
